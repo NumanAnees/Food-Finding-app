f27fd520539d63322c0a1cdcdc1dc5dc0538e266
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-08-06 18:28:55 +0500

    initial changes

--- test/src/App.js
+++ test/src/App.js
@@ -1,7 +1,39 @@
+import React from "react";
 import "./App.css";
-
-function App() {
-  return <h1>hello</h1>;
-}
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/navbar";
+// import Home from "./components/home";
+// import About from "./components/about";
+// import Contact from "./components/contact";
+// import SignIn from "./components/signin";
+// import SignUp from "./components/signup";
+// import Error from "./components/error";
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/About">
+          <About />
+        </Route>
+        <Route exact path="/Contact">
+          <Contact />
+        </Route>
+        <Route exact path="/SignIn">
+          <SignIn />
+        </Route>
+        <Route exact path="/SignUp">
+          <SignUp />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
 
 export default App;

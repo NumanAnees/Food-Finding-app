d4b9a3237d28b7e0273dc7378f5d814358b35ef1
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-02-09 01:50:02 +0500

    initial route setup

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+const authRoutes = require("./routes/auth"); 
+//middlewares
+app.use("/api",authRoutes);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`API is running on port ${port}`));

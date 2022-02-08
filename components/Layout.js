0caf0c4ad871dd91b0177bd4ee4882739dff82ce
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-02-08 17:40:19 +0500

    progressBar added

--- components/Layout.js
+++ components/Layout.js
@@ -1,34 +1,46 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+Router.onRouteChangeStart = url => NProgress.start();
+Router.onRouteChangeComplete = url => NProgress.done();
+Router.onRouteChangeError = url => NProgress.done();
+
+
 const Layout = ({ children }) => {
     const head = () => (
+       <>
         <link
             rel="stylesheet"
             href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
             integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
             crossorigin="anonymous"
         />
+        <link rel="stylesheet" href="/static/styles/style.css" />
+        </>
     );
 
     const nav = () => (
         <ul className="nav nav-tabs bg-dark">
             <Link  href="/">
             <li className="nav-item">
-                <a className="nav-link text-light">
+                <a className="nav-link text-light pointer">
                     Home
                 </a>
             </li>
             </Link>
             <Link href="/login">
             <li className="nav-item">
-                <a className="nav-link text-light" >
+                <a className="nav-link text-light pointer" >
                     Login
                 </a>
             </li>
             </Link>
             <Link href="/register"> 
             <li className="nav-item">
-                <a className="nav-link text-light" >
+                <a className="nav-link text-light pointer" >
                     Register
                 </a>
             </li>

6fe55e0cbdac48c056ddac88c35c2489862c5d6e
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-02-09 15:32:55 +0500

    Register Validation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+//validators
+const {userRegisterValidator} = require("../validators/auth");
+const {runValidation} = require("../validators")
+
+//controllers
 const {register} = require("../controllers/auth")
 
 
-router.post('/register',register);
+router.post('/register',userRegisterValidator,runValidation,register);
 
 module.exports = router; 

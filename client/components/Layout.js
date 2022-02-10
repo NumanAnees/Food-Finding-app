d59241b11af838fdb13d78fd484441dbbbefa075
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-02-10 17:58:14 +0500

    Logout implemented

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,50 +1,73 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Router from 'next/router';
-import NProgress from "nprogress";
-import "nprogress/nprogress.css";
+import { isAuth, logout } from '../helpers/auth';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 Router.onRouteChangeStart = url => NProgress.start();
 Router.onRouteChangeComplete = url => NProgress.done();
 Router.onRouteChangeError = url => NProgress.done();
 
-
 const Layout = ({ children }) => {
     const head = () => (
-       <>
-        <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-            crossOrigin="anonymous"
-        />
-        <link rel="stylesheet" href="/static/styles/style.css" />
+        <>
+            <link
+                rel="stylesheet"
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+                integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+                crossOrigin="anonymous"
+            />
+            <link rel="stylesheet" href="/static/css/styles.css" />
         </>
     );
 
     const nav = () => (
         <ul className="nav nav-tabs bg-dark">
-            <Link  href="/">
             <li className="nav-item">
-                <a className="nav-link text-light pointer">
-                    Home
-                </a>
+                <Link href="/">
+                    <a className="nav-link text-light">Home</a>
+                </Link>
             </li>
-            </Link>
-            <Link href="/login">
-            <li className="nav-item">
-                <a className="nav-link text-light pointer" >
-                    Login
-                </a>
-            </li>
-            </Link>
-            <Link href="/register"> 
-            <li className="nav-item">
-                <a className="nav-link text-light pointer" >
-                    Register
-                </a>
-            </li>
-            </Link>
+
+            {!isAuth() && (
+                <>
+                    <li className="nav-item" style={{marginLeft:"auto "  }}>
+                        <Link href="/login">
+                            <a className="nav-link text-light">Login</a>
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/register">
+                            <a className="nav-link text-light">Register</a>
+                        </Link>
+                    </li>
+                </>
+            )}
+
+            {isAuth() && isAuth().role === 'admin' && (
+                <li className="nav-item" style={{marginLeft:"auto "}}>
+                    <Link href="/admin">
+                        <a className="nav-link text-light">{isAuth().name}</a>
+                    </Link>
+                </li>
+            )}
+
+            {isAuth() && isAuth().role === 'subscriber' && (
+                <li className="nav-item " style={{marginLeft:"auto "  }}>
+                    <Link href="/user">
+                        <a className="nav-link text-light">{isAuth().name}</a>
+                    </Link>
+                </li>
+            )}
+
+            {isAuth() && (
+                <li className="nav-item pointer">
+                    <a onClick={logout} className="nav-link text-light ">
+                        Logout
+                    </a>
+                </li>
+            )}
         </ul>
     );
 

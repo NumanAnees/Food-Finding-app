44df4321e45e26aadad2816a08eab73e6e59f5e5
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-02-11 17:29:01 +0500

    Invalid Algorithm Bug fix

--- client/pages/admin/index.js
+++ client/pages/admin/index.js
@@ -1,6 +1,8 @@
 import Layout from '../../components/Layout';
 import withAdmin from '../withAdmin';
 
-const Admin = ({ user }) => <Layout>{JSON.stringify(user)}</Layout>;
 
+const Admin = ({ user }) =>{
+  return  <Layout>{JSON.stringify(user)}</Layout>
+}
 export default withAdmin(Admin);

d8ccb85087c9b74991cc71264568e885e04bd226
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-02-14 20:16:45 +0500

    Rich text editor

--- client/pages/admin/category/create.js
+++ client/pages/admin/category/create.js
@@ -1,21 +1,24 @@
+import dynamic from 'next/dynamic';
+const ReactQuill = dynamic(()=>import("react-quill"),{ssr:false});
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { API } from '../../../config';
 import { showSuccessMessage, showErrorMessage } from '../../../helpers/alerts';
 import Layout from '../../../components/Layout';
 import withAdmin from '../../withAdmin';
+import 'react-quill/dist/quill.snow.css';
+
 
 const Create = ({ user, token }) => {
     const [state, setState] = useState({
         name: '',
-        content: '',
         url: "",
         error: '',
         success: '',
         buttonText: 'Create',
     });
-
-    const { name, content,url, success, error, buttonText } = state;
+    const [content,setContent] = useState(""); 
+    const { name,url, success, error, buttonText } = state;
 
     const handleChange = name => e => {
         setState({ ...state, [name]: e.target.value, error: '', success: '' });
@@ -45,6 +48,12 @@ const Create = ({ user, token }) => {
         }
     };
 
+      const handleContent = e => {
+        console.log(e);
+        setContent(e);
+        setState({ ...state, success: '', error: '' });
+    };
+
     const createCategoryForm = () => (
         <form onSubmit={handleSubmit}>
             <div className="form-group">
@@ -53,11 +62,17 @@ const Create = ({ user, token }) => {
             </div>
             <div className="form-group">
                 <label className="text-muted">Content</label>
-                <textarea onChange={handleChange('content')} value={content} className="form-control" required />
-            </div>
+                 <ReactQuill
+                    value={content}
+                    onChange={handleContent}
+                    placeholder="Write something..."
+                    theme="snow"
+                    className="pb-5 mb-3"
+                    style={{ border: '1px solid #666' }}
+                />            </div>
               <div className="form-group">
-                <label className="text-muted">Url</label>
-                <textarea onChange={handleChange('url')} value={url} className="form-control" required />
+                <label className="text-muted">Image Url</label>
+                <input onChange={handleChange('url')} value={url} className="form-control" required />
             </div>
             <div>
                 <button className="btn btn-outline-dark">{buttonText}</button>

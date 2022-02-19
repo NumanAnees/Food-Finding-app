1f0f87a750ece9465a38698c089d56ae31c99388
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-02-19 18:33:43 +0500

    Most popular Client

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,8 +2,61 @@ import Layout from '../components/Layout';
 import axios from 'axios';
 import Link from 'next/link';
 import { API } from '../config';
+import { useState,useEffect } from 'react';
+import moment from 'moment';
 
 const Home = ({ categories }) => {
+    const [state,setState]= useState({
+        links:[]
+    })
+    const {links}=state;
+    useEffect(()=>{
+        loadLinks();
+    },[])
+   const loadLinks = async() =>{
+       const response = await axios.get(`${API}/link/popular`);
+       console.log("Main---",response.data);
+       setState({...state,links:response.data});
+   }
+   
+  const handleCount = async linkId => {
+        const response = await axios.put(`${API}/click-count`, { linkId });
+        loadUpdatedLinks();
+    };
+    const loadUpdatedLinks = async () => {
+        loadLinks();
+    };
+
+   
+    const listOfLinks = () =>{
+       return links.map((l, i) => (
+            <div key={i} className="row alert alert-primary p-2">
+                <div className="col-md-8" onClick={e => handleCount(l._id)}>
+                    <a href={l.url} target="_blank">
+                        <h5 className="pt-2">{l.title}</h5>
+                        <h6 className="pt-2 text-danger" style={{ fontSize: '12px' }}>
+                            {l.url}
+                        </h6>
+                    </a>
+                </div>
+                <div className="col-md-4 pt-2" >
+                    <span className="pull-right">
+                        {moment(l.createdAt).fromNow()} by {l.postedBy.name}
+                    </span>
+                    <br />
+                    {/* <span className="badge text-secondary pull-right">{l.clicks} clicks</span> */}
+                </div>
+                <div className="col-md-12 mt-2" style={{"display":"flex","flexDirection":"row"}}>
+                    <span className="badge text-dark">
+                        {l.price}Rs / {l.gst}
+                    </span>
+                    <span className="badge text-success">{l.category.name}</span>
+                     <span className="badge text-secondary pull-left ml-auto" style={{"marginRight":"18.6rem"}}>{l.clicks} clicks</span>
+
+                </div>
+            </div>
+        ))}
+
     const listCategories = () =>
         categories.map((c, i) => (
             <Link href={`/links/${c.slug}`}>
@@ -37,6 +90,12 @@ const Home = ({ categories }) => {
             </div>
 
             <div className="row">{listCategories()}</div>
+            <div className="row mt-4">
+                <h1 className='ml-4'>Most Popular</h1>
+                <div className='col-md-12'>
+                {listOfLinks()}
+                </div>
+            </div>
         </Layout>
     );
 };

1d3a8a4e07bb551dca1c5d7776784f81669a6401
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-03-04 00:07:39 +0500

    issue #3  resolved

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     publicRuntimeConfig: {
         APP_NAME: 'Top Dish',
         API: 'http://localhost:8000/api',
-        PRODUCTION: true,
+        PRODUCTION: false,
         DOMAIN: 'http://localhost:3000',
     }
 };

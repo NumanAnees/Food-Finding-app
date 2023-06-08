abd04df09794b7be6c4c1020fa484b587ca4189e
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2023-06-08 19:29:25 +0500

    pwa final

--- client/pages/_document.jsx
+++ client/pages/_document.jsx
@@ -16,39 +16,42 @@ export default function Document() {
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
 
-        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+        <link
+          rel="apple-touch-icon"
+          href="/static/icons/touch-icon-iphone.png"
+        />
         <link
           rel="apple-touch-icon"
           sizes="152x152"
-          href="/icons/touch-icon-ipad.png"
+          href="/static/icons/touch-icon-ipad.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/icons/touch-icon-iphone-retina.png"
+          href="/static/icons/touch-icon-iphone-retina.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="167x167"
-          href="/icons/touch-icon-ipad-retina.png"
+          href="/static/icons/touch-icon-ipad-retina.png"
         />
 
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/icons/favicon-32x32.png"
+          href="/static/icons/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/icons/favicon-16x16.png"
+          href="/static/icons/favicon-16x16.png"
         />
         <link rel="manifest" href="/manifest.json" />
         <link
           rel="mask-icon"
-          href="/icons/safari-pinned-tab.svg"
+          href="/static/icons/safari-pinned-tab.svg"
           color="#5bbad5"
         />
         <link rel="shortcut icon" href="/favicon.ico" />
